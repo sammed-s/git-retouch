99136ca345b611ab3cc8f38bb217e5a1a725eec0
sammed sankonatti <142508025+sammed-s@users.noreply.github.com>
2024-01-25 12:13:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,4 @@
 console.log("code in the main branch edited");
 console.log("No clarity")
 console.log("Final touch on the ground")
-console.log("The end is is near")
+console.log("code in the sammed branch")
